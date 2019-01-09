3e7ad6bdd12b6922ebfc7bbe4aff4d01f032031d
Paul <inbox@paulgg.com>
2019-01-08 21:17:39 -0700

    progress of databases

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask_site import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -10,3 +11,11 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}".format(form.username.data, form.remember_me.data))
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
--- flask_site.py
+++ flask_site.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import render_template
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
@@ -8,5 +11,9 @@ def create_app():
     return app
 
 app = create_app()
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
+from app import forms
