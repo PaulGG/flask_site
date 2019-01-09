9dd0781a540cd3567bb3512c3451b2056d20c965
Paul <inbox@paulgg.com>
2019-01-09 01:52:19 -0700

    made login and registration thing

--- flask_site.py
+++ flask_site.py
@@ -4,6 +4,8 @@ from flask import render_template
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +15,8 @@ def create_app():
 app = create_app()
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+login = LoginManager(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
