375193bcff1554365e1c38886c6cf85bc78cef30
Paul <inbox@paulgg.com>
2019-01-09 20:45:30 -0700

    refactored javascript, half the lines

--- static/js/main.js
+++ static/js/main.js
@@ -6,63 +6,30 @@ var econtent = document.getElementById("econtent")
 var footer = document.getElementById("footer")
 var theme_text = document.getElementById("themeText")
 
-if (item != null) {
-    if (item == "dark") {
-        button.checked = true
-        setDark()
-    } else {
-        button.checked = false
-        setLight()
-    }
-}
-
-function setDark() {
-    if (!body.classList.contains("dark")) {
-        body.classList.add("dark")
-    }
-    if (!navbar.classList.contains("navbar-dark")) {
-        navbar.classList.add("navbar-dark")
-    }
-    if (!navbar.classList.contains("bg-dark")) {
-        navbar.classList.add("bg-dark")
-    } 
-    if (!econtent.classList.contains("text-light")) {
-        econtent.classList.add("text-light")
-    }
-    if (!footer.classList.contains("dark")) {
-        footer.classList.add("dark")
-        footer.classList.remove("light")
-    }
+if (item == "dark") {
+    button.checked = true
+    toggle()
     theme_text.textContent = " Dark Mode"
+} else {
+    button.checked = false
+    theme_text.textContent = " Light Mode"
 }
 
-function setLight() {
-    if (body.classList.contains("dark")) {
-        body.classList.remove("dark")
-    }
-    if (navbar.classList.contains("navbar-dark")) {
-        navbar.classList.remove("navbar-dark")
-    }
-    if (navbar.classList.contains("bg-dark")) {
-        navbar.classList.remove("bg-dark")
-    } 
-    if (econtent.classList.contains("text-light")) {
-        econtent.classList.remove("text-light")
-    }
-    if (!footer.classList.contains("light")) {
-        footer.classList.remove("dark")
-        footer.classList.add("light")
-    }
-    theme_text.textContent = " Light Mode"
+function toggle() {
+    body.classList.toggle("dark")
+    navbar.classList.toggle("navbar-dark")
+    navbar.classList.toggle("bg-dark")
+    econtent.classList.toggle("text-light")
+    footer.classList.toggle("dark")
 }
 
 function toggleTheme() {
     if (button.checked) {
         localStorage.setItem("themeSetting", "dark")
-        setDark()
-        
+        theme_text.textContent = " Dark Mode"
     } else {
         localStorage.setItem("themeSetting", "light")
-        setLight()
+        theme_text.textContent = " Light Mode"
     }
+    toggle()
 }
